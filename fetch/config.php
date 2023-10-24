789c8a63fdee712e153e9cc50ad0a1e7bb771319
omkar <omkar@teafweb.com>
2023-10-24 22:17:29 +0530

    2nd commit 2023

--- fetch/config.php
+++ fetch/config.php
@@ -1,22 +1,16 @@
-<?php
-/*
-
-define("WEBSITE", "https://elinguahub.com");
+<?php/*
+define("WEBSITE", "http://localhost/elinguahub.com");
 define("SERVER", "localhost");
-define("USERNAME", "elinguah");
-define("PASSWORD", "hostC!@$$");
-define("DATABASE", "elinguah_elingua");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DATABASE", "elinguahub_db");*/
+?> 
+<?php
+define("WEBSITE", "http://localhost/elinguahub.com");
+define("SERVER", "localhost");
+define("USERNAME", "u702940361_elinguahub");
+define("PASSWORD", "SSYeM2y=");
+define("DATABASE", "u702940361_elinguahub_db");
+?> 
 
 
-
-?>*/
-
-define('SERVER','localhost');
-define('USERNAME','root');
-define('PASSWORD', "");
-define('DATABASE', 'elinguahub_db');
-define('WEBSITE','https://elinguahub.com');
-define('EMAIL','admin@elinguahub.com');
-
-
-?>
